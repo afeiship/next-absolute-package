76e5b3cb6b81a66bebcd714db685a4901cbaa8c8
afeiship <1290657123@qq.com>
2020-01-02 19:13:04 +0800

    fix: no build

--- dist/next-absolute-package.js
+++ dist/next-absolute-package.js
@@ -3,16 +3,19 @@
  * description: Get root path package.json.
  * url: https://github.com/afeiship/next-absolute-package
  * version: 1.0.0
- * date: 2019-12-17 00:00:38
+ * date: 2020-01-02 19:12:57
  * license: MIT
  */
 
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
+  var path = require('path');
 
-  nx.absolutePackage = function() {
-    //code goes here.
+  nx.absolutePackage = function(inPath) {
+    var appPath = require('app-root-path').path;
+    var pkg = require(path.join(appPath, 'package.json'));
+    return inPath ? nx.get(pkg, inPath) : pkg;
   };
 
   if (typeof module !== 'undefined' && module.exports) {
